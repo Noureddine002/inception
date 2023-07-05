8c0b547f38aad9620fec314188acce5d33cfa182
Noureddine Tanjaoui <noureddine.nt3@gmail.com>
2023-07-05 19:28:59 +0100

    almost done

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,8 +1,8 @@
 service mysql start;
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_UPASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_UPASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,32 @@
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+
+./wp-cli.phar core download  --path="/var/www/html/" --allow-root
+
+sleep 10
+
+./wp-cli.phar config create \
+    --dbhost=mariadb        \
+    --dbname=$DB_NAME       \
+    --dbuser=$DB_USER       \
+    --dbpass=$DB_UPASSWORD  \
+    --path=/var/www/html/ --allow-root
+
+./wp-cli.phar core install              \
+    --url=$URL                          \
+    --title=$TITLE                      \
+    --admin=$ADMIN                      \
+    --admin_password=$SQL_ROOT_PASSWORD \
+    --admin_email=$EMAIL                \
+    --path=/var/www/html/ --allow-root
+
+./wp-cli.phar user create $DB_USER $EMAIL   \
+    --role=author                           \
+    --user_pass=$DB_UPASSWORD               \
+    --path=/var/www/html/ --allow-root
+
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+
+mkdir -p /run/php
+
+php-fpm7.3 -F 
